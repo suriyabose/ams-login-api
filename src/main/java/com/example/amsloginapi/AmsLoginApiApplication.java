a8b09e12f7f159ddfcc1dabd6a33d0a92acbac2d
suriyabose <suriyakumarbose@gmail.com>
2022-06-01 16:33:21 +0530

    init_commit

---
+++ src/main/java/com/example/amsloginapi/AmsLoginApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.amsloginapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsLoginApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmsLoginApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/amsloginapi/AmsLoginApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amsloginapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmsLoginApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
